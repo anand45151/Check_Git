813b9c7cb4a888fab47618de13efca28e3a59ec6
Anand <83801261+anand45151@users.noreply.github.com>
2024-06-23 21:39:33 +0530

    Here the First commit

---
+++ arr.js
@@ -0,0 +1,6 @@
+const arr = [12,12,32,23,43]
+console.log(arr);
+
+const newar = arr.join()
+console.log(newar)
+// console.log(arr.includes(45))
---
+++ index.js
@@ -0,0 +1,19 @@
+let name = "ANAND";
+const add = "SURAT";
+let num = "90909"
+// console.table([name,add,num]);
+obj = {
+    name:"ANAND",
+    roll :"21BTX",
+    add:"SUurat"
+}
+obj2 = obj ; 
+obj2.name = "ANAND KUMAR";
+console.log(obj);
+
+const getname = new String ('ANAND YADAV RAMSINGH')
+console.log(getname.charAt(2));  
+console.log(getname.indexOf('R'));  
+
+const number = new Number(500)
+console.log(number);
